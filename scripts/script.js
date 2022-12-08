571c1889c2d21b5edb296dc0929ca72f7301d64c
LilianTalsma <118172231+LilianTalsma@users.noreply.github.com>
2022-12-08 10:46:01 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,14 @@
 // JavaScript Document
 console.log("hi");
+
+let deMenuButton = document.querySelector('header > button');
+let deWaas = document.querySelector('header div');
+let deNav = document.querySelector('header nav');
+
+deMenuButton.addEventListener('click', toggleMenu);
+deWaas.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+    deNav.classList.toggle('open');
+    deWaas.classList.toggle('open');
+  }
